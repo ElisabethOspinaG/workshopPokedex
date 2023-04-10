309c37c70c87618947f3324e3d25391be7dec174
lizeth montoya <lizethmontoyagarcia@gmail.com>
2023-04-09 23:39:30 -0500

    Refactorizar el codigo creando una funcion para pintar la tabla

--- script.js
+++ script.js
@@ -16,6 +16,50 @@ const getPokemonsfromapi = async (url) => {
     return data;
 };
 
+    /*----------------- SINCRONIZAR LA INFORMACION DEL POKEMON CARGADO ALEATORIAMENTE LIZ -------------------*/
+function printInfoPokemonInTable(pokemonInfo) {
+
+  // Para pintra la tabla voy a usar la constante dataPokemon construida en la linea 24
+  const idPokemonElement = document.getElementById("No-Pokemon-Random");
+  idPokemonElement.innerHTML = `${pokemonInfo.id}`;
+
+  // PINTAR EL LEVEl
+  const levelPokemon = pokemonInfo.base_experience;
+  //console.log("nivel del pokemon ", levelPokemon);
+  const levelPokemonElement = document.getElementById("level-pokemon-random");
+  levelPokemonElement.innerHTML = `${levelPokemon}`;
+
+  // PINTAR EL TYPE
+  const typePokemon = pokemonInfo.types[0].type.name;
+  //console.log("tipo de pokemon ", typePokemon);
+  const typePokemonElement = document.getElementById("type-pokemon-random");
+  typePokemonElement.innerHTML = `${typePokemon}`;
+
+  // PINTAR EL HABILITY (ES MAS DE UNA HABILIDAD , MOSTRAR 4 MAx)
+  let abilityName = "";
+  pokemonInfo.abilities.forEach((ability) => {
+    abilityName += ability.ability.name + " , ";
+  });
+  //console.log("habilidades del pokemon", abilityName);
+  const abilitiesPokemonElement = document.getElementById(
+    "hability-pokemon-random"
+  );
+
+  abilitiesPokemonElement.innerHTML = `${abilityName}`;
+
+  // PINTAR EL HIGHT
+  const heightPokemon = pokemonInfo.height;
+  //console.log("la altura del pokemon es ", heightPokemon);
+  const heightPokemonElement = document.getElementById("height-pokemon-random");
+  heightPokemonElement.innerHTML = `${heightPokemon}`;
+
+  // PINTAR EL WEIGGHT
+  const weightPokemon = pokemonInfo.weight;
+  //console.log("el peso del pokemon es ", weight);
+  const weightPokemonElement = document.getElementById("weight-pokemon-random");
+  weightPokemonElement.innerHTML = `${weightPokemon}`;
+}
+
 
 document.addEventListener("DOMContentLoaded", async () => {
   const infoapiname = await getPokemonsfromapi(URL_API);
@@ -44,47 +88,8 @@ document.addEventListener("DOMContentLoaded", async () => {
 
   
 
-  /*----------------- SINCRONIZAR LA INFORMACION DEL POKEMON CARGADO ALEATORIAMENTE LIZ -------------------*/
-
-  // Para pintra la tabla voy a usar la constante dataPokemon construida en la linea 24
-  const idPokemonElement = document.getElementById("No-Pokemon-Random");
-  idPokemonElement.innerHTML = `${dataPokemon.id}`;
-
-  // PINTAR EL LEVEl
-  const levelPokemon = dataPokemon.base_experience;
-  //console.log("nivel del pokemon ", levelPokemon);
-  const levelPokemonElement = document.getElementById("level-pokemon-random");
-  levelPokemonElement.innerHTML = `${levelPokemon}`;
-
-  // PINTAR EL TYPE
-  const typePokemon = dataPokemon.types[0].type.name;
-  //console.log("tipo de pokemon ", typePokemon);
-  const typePokemonElement = document.getElementById("type-pokemon-random");
-  typePokemonElement.innerHTML = `${typePokemon}`;
-
-  // PINTAR EL HABILITY (ES MAS DE UNA HABILIDAD , MOSTRAR 4 MAx)
-  let abilityName = "";
-  dataPokemon.abilities.forEach((ability) => {
-    abilityName += ability.ability.name + " , ";
-  });
-  //console.log("habilidades del pokemon", abilityName);
-  const abilitiesPokemonElement = document.getElementById(
-    "hability-pokemon-random"
-  );
-
-  abilitiesPokemonElement.innerHTML = `${abilityName}`;
-
-  // PINTAR EL HIGHT
-  const heightPokemon = dataPokemon.height;
-  //console.log("la altura del pokemon es ", heightPokemon);
-  const heightPokemonElement = document.getElementById("height-pokemon-random");
-  heightPokemonElement.innerHTML = `${heightPokemon}`;
-
-  // PINTAR EL WEIGGHT
-  const weightPokemon = dataPokemon.weight;
-  //console.log("el peso del pokemon es ", weight);
-  const weightPokemonElement = document.getElementById("weight-pokemon-random");
-  weightPokemonElement.innerHTML = `${weightPokemon}`;
+// llamando la funcion para llenar la tabla LIZ 
+printInfoPokemonInTable (dataPokemon);
 
   const imagenesPokemonesElemento = document.querySelector(".imagenes-pokemones-todos");
 
@@ -108,8 +113,6 @@ for (let i = 0; i < pokemones.length; i++) {
 
 
 // ----------------------------------- APLICAR FUNCIONALIDAD / DIEGO ------------------------------
-
-
   
     // Reemplazar la imagen principal con la del Pokemon seleccionado
     const imagenPokemonPrincipal = document.getElementById("imagen-pokemon");
@@ -120,33 +123,9 @@ for (let i = 0; i < pokemones.length; i++) {
   const nombrePokemonElemento = document.getElementById("nombre-pokemon");
   nombrePokemonElemento.innerHTML = `${nombrePokemon.toUpperCase()}`;
   
-    // Reemplazar la información de la tabla con la del Pokemon seleccionado
-    const idPokemonElement = document.getElementById("No-Pokemon-Random");
-    idPokemonElement.innerHTML = `${pokemonDetail.id}`;
-  
-    const levelPokemonElement = document.getElementById("level-pokemon-random");
-    levelPokemonElement.innerHTML = `${pokemonDetail.base_experience}`;
-  
-    const typePokemonElement = document.getElementById("type-pokemon-random");
-    typePokemonElement.innerHTML = `${pokemonDetail.types[0].type.name}`;
-  
-    let abilityName = "";
-    pokemonDetail.abilities.forEach((ability) => {
-      abilityName += ability.ability.name + " , ";
-    });
-    const abilitiesPokemonElement = document.getElementById(
-      "hability-pokemon-random"
-    );
-    abilitiesPokemonElement.innerHTML = `${abilityName}`;
-  
-    const heightPokemonElement = document.getElementById("height-pokemon-random");
-    heightPokemonElement.innerHTML = `${pokemonDetail.height}`;
-  
-    const weightPokemonElement = document.getElementById("weight-pokemon-random");
-    weightPokemonElement.innerHTML = `${pokemonDetail.weight}`;
-
-
-
+    //------- Reemplazar la información de la tabla con la del Pokemon seleccionado con la funcion liz----------------
+    
+   printInfoPokemonInTable (pokemonDetail);
   });
   
 }
